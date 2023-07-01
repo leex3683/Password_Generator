bcad718d5adfe000ad120a12313f8236d0bb3182
unknown <leex3683@umn.edu>
2023-07-01 15:57:46 -0500

    add pop ups requesting password chars and number of chars

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // Assignment code here
+let upperLib = "QWERTYUIOPASDFGHJKLZXCVBNM";
+let lowerLib = "qwertyuiopasdfghjklzxcvbnm";
+let numLib = "1234567890";
+let speLib =  ' !"#$%&' + "()*+,-./:;<=>?@[\]^_`{|}~";
+function generatePassword(){
+  charnum = prompt("Enter Number of Characters 8 - 128");
+  if(charnum == null){return;};
+  boolUpper = confirm("Do you want upper case characters? Choose 'ok' to include, or 'cancel' to exclude");
+  boolLower = confirm("Do you want lower case characters? Choose 'ok' to include, or 'cancel' to exclude");
+  boolNum = confirm("Do you want numbers? Choose 'ok' to include, or 'cancel' to exclude");
+  boolSpe = confirm("Do you want special characters? Choose 'ok' to include, or 'cancel' to exclude");
+
+}
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password =  generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
